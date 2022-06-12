820360105591a5e806fcac6d6635d9aa17036159
sanyamgoel10 <57743195+sanyamgoel10@users.noreply.github.com>
2022-06-12 12:32:28 +0530

    update host 0.0.0.0

--- UI/main.py
+++ UI/main.py
@@ -135,4 +135,4 @@ def viewticket():
 ##  viewBooking.html and viewOrder.html are currently not being used
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 4000)
+    app.run(host='0.0.0.0',debug=True, port = 4000)
